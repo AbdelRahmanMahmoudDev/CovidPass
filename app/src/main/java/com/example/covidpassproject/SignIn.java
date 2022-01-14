a24d5379dcf9ffdff3b055c2b2a9a12de88e1d4b
AHFury1918 <94147850+AHFury1918@users.noreply.github.com>
2022-01-14 23:49:58 +0200

    NAV VIEW

--- app/src/main/java/com/example/covidpassproject/SignIn.java
+++ app/src/main/java/com/example/covidpassproject/SignIn.java
@@ -39,7 +39,7 @@ public class SignIn extends AppCompatActivity {
 
                 // Temporary path for testing google maps api
                 // startActivity(new Intent(SignIn.this, MapsActivity.class));
-                startActivity(new Intent(SignIn.this, MainActivity.class));
+                startActivity(new Intent(SignIn.this, QRCodeScanner.class));
 
                 //String Email = email.getText().toString();
                 //String Password = password.getText().toString();
