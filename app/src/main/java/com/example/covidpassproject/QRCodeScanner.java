895eedd757a73d522e96ec6e5872e327daeba81c
AHFury1918 <abdallahossam2105@student.aast.edu>
2022-01-15 16:36:14 +0200

    df

--- app/src/main/java/com/example/covidpassproject/QRCodeScanner.java
+++ app/src/main/java/com/example/covidpassproject/QRCodeScanner.java
@@ -95,7 +95,7 @@ public class QRCodeScanner extends FragmentActivity {
                     @Override
                     public void run() {
                         t1.setText(result.getText().substring(0, 4));
-                        t2.setText(result.getText().substring(result.getText().length() - 16));
+                        t2.setText(result.getText().substring(result.getText().length() - 14));
                         popup.hide();
                     }
                 });
